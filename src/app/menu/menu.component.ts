d9d8b2fefb6ce6ae16ccc7ab88c91211307d1bc8
petry078@gmail.com <petry078@gmail.com>
2022-04-04 00:08:45 -0300

    só falta o vídeo dos temas

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
+
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  nome = environment.nome
+  foto = environment.foto
 
-  ngOnInit(): void {
+  constructor(private router: Router) {}
+
+  ngOnInit(){}
+
+  sair(){
+    this.router.navigate(["/login"])
+    environment.token = ""
+    environment.nome = ""
+    environment.foto = ""
+    environment.id = 0
   }
 
 }
